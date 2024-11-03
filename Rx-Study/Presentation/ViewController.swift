8cdb5fbc53acc0d2844b1de995d30b9d1eab4f17
mmaybei <yebbi2@ewhain.net>
2024-11-04 01:54:12 +0900

    [feat] 텍스트필드 추가

--- Rx-Study/Presentation/ViewController.swift
+++ Rx-Study/Presentation/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    private let textField = UITextField().then {
+        $0.font = .systemFont(ofSize: 16, weight: .medium)
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.systemGray4.cgColor
+        $0.layer.cornerRadius = 8
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
+        setLayout()
     }
+}
 
+private extension ViewController {
+    func setUI() {
+        view.backgroundColor = .white
+        view.addSubview(textField)
+    }
+    
+    func setLayout() {
+        textField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(120)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
+    }
 }
